8ab8dc7bb89ad4a0e08dd0fce4385c7843709c5c
Miguel <a16001614@alumnos.uady.mx>
2021-11-30 14:12:38 -0600

    Listo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo!");
+        System.out.println("Verificacion");
     }
 }
