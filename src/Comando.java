e8a1e05c38c73f62ee39b8196aa5fd6f90461955
MiguelHGH <a16001614@alumnos.uady.mx>
2021-12-05 19:55:11 -0600

    Lector, verificacion basica de la estructura comandos

---
+++ src/Comando.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Comando {
+    private final Scanner cmdscan;
+    public Comando(){
+        cmdscan = new Scanner(System.in);
+    }
+
+    public String RecibirCmd(){
+        System.out.println("Introduzca el comando");
+        return cmdscan.nextLine();
+    }
+}
